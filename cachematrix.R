6131412af3f3affe4ce2adaa4d4ede8ce9eeb247
Gustavo Lobo <gustavolobo90@gmail.com>
2014-08-22 20:36:28 +0100

    Completed programming assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file has two methods with the objective of improving the
+## performance of repeated inverse matrix calculations.
+## On the first time that we calculate the inverse of a matrix,
+## we save its inversible matrix. By the second time (and others),
+## we use the cached value.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function holds other four functions and a variable.
+## The variable is initialized as NULL until the first time we
+## cache the inverted matrix.
+## The other functions allow us to:
+## set               - initializes/reinitializes the matrix
+## get               - returns the matrix
+## setInvertedMatrix - caches the inverted matrix passed as an 
+##                     argument into a variable called
+##                     cachedInvertedMatrix
+## getInvertedMatrix - returns the value of cachedInvertedMatrix
+makeCacheMatrix <- function(originalMatrix = matrix()) {
+  cachedInvertedMatrix <- NULL
+  set <- function(y) {
+    originalMatrix <<- y
+    cachedInvertedMatrix <<- NULL
+  }
+  get <- function() originalMatrix
+  setInvertedMatrix <- function(invertedMatrix) {
+    cachedInvertedMatrix <<- invertedMatrix
+  }
+  getInvertedMatrix <- function() cachedInvertedMatrix
+  list(set = set,
+       get = get,
+       setInvertedMatrix = setInvertedMatrix,
+       getInvertedMatrix = getInvertedMatrix)
 }
 
-
-## Write a short comment describing this function
-
+## This function returns the inverted matrix by returning the cached
+## one, or if there is no cached matrix, the function will calculate
+## it, save it for later and return it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invertedMatrix <- x$getInvertedMatrix()
+  if(!is.null(invertedMatrix)) {
+    return(invertedMatrix)
+  }
+  originalMatrix <- x$get()
+  invertedMatrix <- solve(originalMatrix, ...)
+  x$setInvertedMatrix(invertedMatrix)
+  invertedMatrix
 }
